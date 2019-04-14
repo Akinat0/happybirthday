ce202a0ed88de37ba4b2934e97f9be6c32771084
Akinat0 <Akinat0@users.noreply.github.com>
2019-04-14 13:28:11 +0300

    Init

---
+++ Assets/Script/Beh.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beh : MonoBehaviour
+{
+
+    private StateMachine stateMachine;
+
+
+
+    private void Awake()
+    {
+        stateMachine = FindObjectOfType<StateMachine>().GetComponent<StateMachine>();
+
+        if (stateMachine != null) print("I found state machine");
+
+    }
+    private void OnEnable()
+    {
+        stateMachine.addTracker();
+    }
+
+    private void OnDisable()
+    {
+        stateMachine.removeTracker();
+    }
+}
---
+++ Assets/Script/StateMachine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject scannerScreen;
+
+    private int tracked = 0; 
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (tracked<=0)
+        {
+            scannerScreen.SetActive(true);
+        }
+        else {
+            scannerScreen.SetActive(false);
+        }
+    }
+
+    public void addTracker()
+    {
+        tracked++;
+    }
+
+    public void removeTracker()
+    {
+        tracked--;
+    }
+}
