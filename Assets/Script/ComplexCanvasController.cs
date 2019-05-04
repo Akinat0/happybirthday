4ac8b383208086e1e64454805c963e58ba840eba
Akinat0 <Akinat0@users.noreply.github.com>
2019-05-04 13:24:57 +0300

    Golems work almost correctly

---
+++ Assets/Script/ComplexCanvasController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class ComplexCanvasController : MonoBehaviour {
+
+    public GameObject[] golems;
+    public GameObject[] btns;
+
+    public void hideGolem() {
+        foreach (var go in golems) go.SetActive(false);
+        foreach (var go in btns) go.SetActive(true);
+        Debug.Log("Golem hided");
+    }
+
+    // Use this for initialization
+    void Start () {
+        foreach (var go in golems) go.SetActive(false);
+        foreach (var go in btns) go.SetActive(true);
+        if (golems.Length != btns.Length) Debug.LogWarning("Quantity of golems and btns isn't equal");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void click(int index) {
+        //Disable all other buttons
+        for (int i = 0; i < btns.Length; i++) {
+            if (i != index) {
+                btns[i].SetActive(false);
+            }
+        }
+
+        golems[index].SetActive(true);
+    }
+}
+[System.Serializable]
+public class HideGolemEvent : UnityEvent {}
---
+++ Assets/Script/PlateController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlateController : MonoBehaviour {
+
+
+    public HideGolemEvent hideGolem;
+    public GameObject[] plates;
+    private int index = 0;
+
+
+
+    // Use this for initialization
+    void Start() {
+        foreach (var g in plates) g.SetActive(false);
+        plates[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void onClick() {
+        Debug.Log("On click");
+        plates[index].SetActive(false);
+        if (index == plates.Length - 1) {
+            hideGolem.Invoke();
+            return;
+        }
+        index++;
+        plates[index].SetActive(true);
+    }
+}
